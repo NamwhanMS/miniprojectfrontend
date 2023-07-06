384ba6142b6dde2fc974740f10a212fbf16905e4
NamwhanMS <138745656+NamwhanMS@users.noreply.github.com>
2023-07-06 11:48:37 +0700

    add page

---
+++ src/components/Page404.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Not Found</h1>
+    <RouterLink to="/">Back to Homepage</RouterLink>
+</template>
--- src/plugins/routes.ts
+++ src/plugins/routes.ts
@@ -1,12 +1,17 @@
 import  { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '../components/PageHome.vue'
 import PageLogin from '../components/PageLogin.vue'
+import PageRegister from '../components/PageRegister.vue'
+import Page404 from '../components/Page404.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: PageHome },
-        { path: '/', component: PageLogin }
+        { path: '/login', component: PageLogin },
+        { path: '/register', component: PageRegister },
+        { path: '/:pathMatch(.*)*',  component: Page404 }
+        
     ]
 
 })
