363d3c6a82ccb6cdaf9aed4bd033b0713e21b15d
NamwhanMS <138745656+NamwhanMS@users.noreply.github.com>
2023-07-11 10:43:29 +0700

    check Pagelogin

---
+++ src/store/user.ts
@@ -0,0 +1,48 @@
+import { computed, reactive } from 'vue'
+import * as Request from '../requests'
+//import { useRouter } from 'vue-router'
+
+//const router = useRouter() 
+
+const state = reactive({
+  name: '',
+  email: '',
+
+  error: ''
+
+})
+
+const getters = reactive({
+  isLoggedIn: computed(() => state.email !== '')
+})
+
+const actions = {
+  async getUser() {
+    const user = await Request.getUser()
+    if (user == null) return
+
+    state.name = user.name
+    state.email = user.email
+  },
+  async login(email: string, password: string) {
+    const user = await Request.login(email, password)
+    if (user == null) {
+      state.error = 'Could not find user.' 
+      return false
+    }
+
+    state.name = user.name
+    state.email = email
+    state.error = ''
+    //console.log(router)
+    //router.push('/')
+    return true
+    
+  },
+  async logout() {
+    state.name = ''
+    state.email = ''
+  }
+}
+
+export default { state, getters, ...actions }
