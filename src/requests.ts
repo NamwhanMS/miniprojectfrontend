580fc3cf3fd4193562e4020dd73fe1b0c6a037bb
NamwhanMS <138745656+NamwhanMS@users.noreply.github.com>
2023-07-11 18:01:31 +0700

    get users

--- src/requests.ts
+++ src/requests.ts
@@ -1,14 +1,19 @@
-export type User = { name: string; email: string }
-export type UserList = Array<User & { password: string }>
+export type User = { name: string; email: string };
+export type UserList = Array<User & { password: string }>;
 
 export async function getUser() {
-  const request = await fetch('/current.json')
-  const user: User = await request.json()
-  return user
+  const request = await fetch("/current.json");
+  const user: User = await request.json();
+  return user;
 }
 
 export async function login(email: string, password: string) {
-  const request = await fetch('/users.json')
-  const usersJson: UserList = await request.json()
-  return usersJson.find(s => s.password === password && s.email === email)
+  const request = await fetch("/users.json");
+  const response = await request.json();
+  console.log(response);
+  if (response) {
+    const usersJson: UserList = response.users;
+    console.log(usersJson);
+    return usersJson.find((s) => s.password === password && s.email === email);
+  }
 }
